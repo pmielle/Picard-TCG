77df5e36ae6f7b8c5b1a66445ed211a667f9050d
Paul Mielle <pm.mielle@gmail.com>
2025-01-10 17:37:38 +0100

    card has all its attr

--- Assets/ScriptableObjects/CardData.cs
+++ Assets/ScriptableObjects/CardData.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 public class CardData : ScriptableObject
 {
     public new string name;
+    public float price;
+    public string quote;
+    public string review;
+    public string collection;
+    public int year;
+    public string author;
 }
--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,10 +5,22 @@ public class Card : MonoBehaviour
 {
     public CardData cardData;
     public new GameObject name;
+    public GameObject price;
+    public GameObject quote;
+    public GameObject review;
+    public GameObject collection;
+    public GameObject year;
+    public GameObject author;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         name.GetComponent<TextMeshProUGUI>().text = cardData.name;
+        price.GetComponent<TextMeshProUGUI>().text = cardData.price.ToString();
+        quote.GetComponent<TextMeshProUGUI>().text = $"\"{cardData.quote}\"";
+        review.GetComponent<TextMeshProUGUI>().text = cardData.review;
+        collection.GetComponent<TextMeshProUGUI>().text = cardData.collection;
+        year.GetComponent<TextMeshProUGUI>().text = cardData.year.ToString();
+        author.GetComponent<TextMeshProUGUI>().text = cardData.author;
     }
 }
