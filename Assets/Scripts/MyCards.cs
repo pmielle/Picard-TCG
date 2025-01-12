9a41c339ec811220f71d6f8a24f68c87c57d8a17
Paul Mielle <pm.mielle@gmail.com>
2025-01-12 11:59:36 +0100

    stack cards

--- Assets/Scripts/MyCards.cs
+++ Assets/Scripts/MyCards.cs
@@ -15,13 +15,54 @@ public class MyCards : MonoBehaviour
         var content = GameObject.Find("Content");
         // spawn cards
         var card = Resources.Load("CardInList");
-        foreach (var cardId in player.cards)
+        var cards = player.cards;
+        cards.Sort();  // need to be sorted for stacking
+        int? currentCardId = null;
+        int currentStackCount = 0;
+        foreach (var cardId in cards)
         {
-            var cardData = Resources.Load<CardData>(cardId.ToString());
-            var clone = Instantiate(card, content.transform);
-            clone.GetComponent<Card>().cardData = cardData;
-            clone.GetComponent<Button>().onClick.AddListener(() => GoToCard(cardId));
+            // only happens for the first card
+            if (currentCardId == null)
+            {
+                currentCardId = cardId;
+                currentStackCount = 1;
+                continue;
+            }
+            // for every other card:
+            else 
+            { 
+                // the card is the same as the previous one: increment the stack counter
+                if (currentCardId == cardId) 
+                {
+                    currentStackCount += 1;
+                    continue;
+                }
+                // this is a new card: spawn the previous card (with its stack count) and start a new stack count for the new card
+                else
+                {
+                    spawn((int)currentCardId, currentStackCount, card, content);
+                    // setup new card
+                    currentCardId = cardId;
+                    currentStackCount = 1;
+                }
+            }
         }
+        // spawn the last card (happens everytime player.cards is not empty)
+        if (currentCardId != null)
+        {
+            spawn((int)currentCardId, currentStackCount, card, content);
+            // no need to reset currentCardId and currentStackCount, we are done here
+            return;
+        }
+    }
+
+    void spawn(int cardId, int stackCount, Object card, GameObject content)
+    {
+        Debug.Log($"stack count is {stackCount}");
+        var cardData = Resources.Load<CardData>(cardId.ToString());
+        var clone = Instantiate(card, content.transform);
+        clone.GetComponent<Card>().cardData = cardData;
+        clone.GetComponent<Button>().onClick.AddListener(() => GoToCard(cardId));
     }
 
     public void GoToCard(int cardId) {
